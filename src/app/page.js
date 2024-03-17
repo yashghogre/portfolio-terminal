fcd5b454a84a5ab9ee24677f67d612ea05e0bc05
Yash <yashghogre100@gmail.com>
2024-03-17 23:57:04 +0530

    minor changes

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-[#181818]">
-      <div className="border-2 border-white p-10 w-[80vw] rounded-lg">
+      <div className="border-2 border-t-8 border-white p-10 w-[80vw] rounded-lg">
         <p className="text-center">
           ██╗░░░██╗░█████╗░░██████╗██╗░░██╗  ░██████╗░██╗░░██╗░█████╗░░██████╗░██████╗░███████╗
           ╚██╗░██╔╝██╔══██╗██╔════╝██║░░██║  ██╔════╝░██║░░██║██╔══██╗██╔════╝░██╔══██╗██╔════╝
@@ -11,6 +11,8 @@ export default function Home() {
           ░░╚██╔╝░░██╔══██║░╚═══██╗██╔══██║  ██║░░╚██╗██╔══██║██║░░██║██║░░╚██╗██╔══██╗██╔══╝░░
           ░░░██║░░░██║░░██║██████╔╝██║░░██║  ╚██████╔╝██║░░██║╚█████╔╝╚██████╔╝██║░░██║███████╗
           ░░░╚═╝░░░╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝  ░╚═════╝░╚═╝░░╚═╝░╚════╝░░╚═════╝░╚═╝░░╚═╝╚══════╝</p>
+          <p className="text-center">Welcome to my Portfolio. This is a terminal-style portfolio Site.</p>
+          <input className="bg-[#101010]" />
       </div>
     </main>
   );
